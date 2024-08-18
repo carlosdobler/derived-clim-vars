e6f4156e4971e1e9a20147eedc1dfcd375b0612d
cdobler-risk-vm-05 <dobler.carlos@gmail.com>
2024-08-18 17:47:15 +0000

    adrop edit and eq proof added

--- vpd_mon_era5.R
+++ vpd_mon_era5.R
@@ -31,7 +31,7 @@ fs::dir_create(dir_temp)
 time_vector <- 
   seq(as_date("1995-01-01"), as_date("2020-12-01"), by = "1 month")
 
-# all input files
+# fetch all input file names
 ff_tas <- 
   str_glue("gsutil ls {dir_data}/2m_temperature") %>% 
   system(intern = T) %>% 
@@ -51,12 +51,14 @@ ff_dewp <-
   str_subset(".nc")
 
 
-# ARE BOTH LIST OF FILES THE SAME LENGTH?
+# ARE BOTH LIST OF FILE NAMES THE SAME LENGTH?
 length(ff_tas) == length(ff_dewp)
 
 
-
-for (d in time_vector) {
+# loop through all dates
+for (d in as.character(time_vector)) {
+  
+  print(d)
   
   f <- 
     c(ff_tas %>% 
@@ -65,19 +67,22 @@ for (d in time_vector) {
       ff_dewp %>% 
         str_subset(as.character(d)))
   
+  # copy files to disk
   f %>% 
     walk(~str_glue("gsutil cp {.x} {dir_temp}") %>% 
            system(ignore.stdout = T, ignore.stderr = T))
   
+  # read files
   s <- 
     dir_temp %>% 
     fs::dir_ls() %>% 
     map(read_ncdf, proxy = F) %>% 
     suppressMessages() %>%
+    map(adrop) %>% 
     unname() %>% 
-    do.call(c, .) %>% 
-    adrop()
+    do.call(c, .)
   
+  # calculate vpd
   s_vpd <- 
     s %>% 
     mutate(d2m = d2m %>% units::set_units(degC),
@@ -93,32 +98,57 @@ for (d in time_vector) {
     
     select(vpd)
   
+  # save result
   rt_write_nc(s_vpd, 
               str_glue("{dir_temp}/era5_vapor-pressure-deficit_mon_{d}.nc"),
               gatt_name = "source code",
-              gatt_val = "lkhfgjhgslkd")
-  
-  
-  
-  # add comment to file?
+              gatt_val = "https://github.com/carlosdobler/derived-clim-vars/blob/main/vpd_mon_era5.R")
   
   # transfer to bucket
-  str_glue("gsutil mv {dir_temp}/era5_vapor-pressure-deficit_mon_{d}.nc {dir_data}/vapor_pressure_deficit")
+  str_glue("gsutil mv {dir_temp}/era5_vapor-pressure-deficit_mon_{d}.nc {dir_data}/vapor_pressure_deficit") %>% 
+    system(ignore.stdout = T, ignore.stderr = T)
   
   # delete temp files
+  dir_temp %>% 
+    fs::dir_ls() %>% 
+    fs::file_delete()
   
 }
 
-
-
+# delete temporary dir
 fs::dir_delete(dir_temp)
 
 
 
 
 
-
-
+# # EQUATION PROOF:
+# 
+# # From TDEW to RH
+# tdew = 6.2113
+# t2m = 25
+# 100 * exp((17.62 * tdew) / (243.12 + tdew)) / exp(17.62 * t2m / (243.12 + t2m)) # RH ~30
+# 
+# # From RH to TDEW
+# rh = 30
+# t2m = 25
+# 243.04 * (log(rh/100) + ((17.625 * t2m) / (243.04 + t2m))) / (17.625 - log(rh/100) - ((17.625 * t2m) / (243.04 + t2m))) # tdew ~ 6.2213
+# # Source: https://bmcnoldy.earth.miami.edu/Humidity.html
+# 
+# 
+# # From RH and TEMP to TDEW
+# rh = 30
+# t2m = 25
+# 
+# es = 0.6112 * exp((17.62 * t2m) / (243.12 + t2m)) # saturation vapor pressure (tetens)
+# es * (1 - rh/100) # vpd ~ 2.2120
+# # Source: ESSD paper linked above
+# 
+# # Alternative:
+# es = 0.6112 * exp((17.62 * t2m) / (243.12 + t2m)) # saturation vapor pressure (tetens?)
+# ea = rh / 100 * es # actual vapor pressure
+# es-ea # vpd ~ 2.2120
+# # Source: https://physics.stackexchange.com/questions/4343/how-can-i-calculate-vapor-pressure-deficit-from-temperature-and-relative-humidit
 
 
 
